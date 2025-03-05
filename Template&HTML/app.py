bf420176dc8c81739f3616ffc4ed9a3aa4d5773d
tanish <tanish.jain.0905@gmail.com>
2025-03-05 23:59:40 +0530

    learning_5_march

---
+++ Template&HTML/app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,redirect,url_for
+
+app = Flask(__name__,template_folder='templates')
+
+@app.route('/')
+def index():
+    myvalue = 'NeuralNine'
+    myresult = 10 + 20
+    mylist =[10,20,30,40,50]
+    return render_template("index.html", mylist=mylist)
+
+
+@app.route('/other')
+def other():
+    some_text = 'Hello world'
+    return render_template('other.html',some_text = some_text)
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter('repeat')
+def repeat(s,times=2):
+    return s*times
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join(c.upper() if i%2==0 else c.lower() for i,c in enumerate(s))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ firstapp/app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello world</h1>"
+
+@app.route('/hello')
+def hello():
+    return 'Hello world\n',
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1,number2):
+
+    return f"{number1}+{number2}  = {number1+number2}"
+
+@app.route('/handle_url_params')
+def handle_params():
+    if('greeting' in request.args.keys() and 'name' in request.args.keys()):
+        greeting = request.args['greeting']
+        name = request.args.get('name')
+        return f'{greeting},{name}'
+    else:
+        return "some parameter are missing"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5555,debug=True)
